ccf4d5cbb8489a41875a80832e3ce5a8e50ef8ce
Kevin Zusy <kzusy@everydayhealthinc.com>
2018-07-27 11:36:20 -0400

    moved the current logic into scheduler.js to get ready for sms integration

---
+++ src/scheduler.js
@@ -0,0 +1,73 @@
+const _ = require('lodash');
+const moment = require('moment');
+const path = require('path');
+const config = require('./config');
+const GoogleCalendar = require('./helpers/google.calendar');
+const TemperatureReader = require('./helpers/temperature.reader');
+
+const googleCalendar = new GoogleCalendar(config.googleCredentials);
+const temperatureReader = new TemperatureReader(config.tempUrl);
+
+const INTERVAL_TIME = 5 * 60 * 1000;
+
+const updatedEvents = {};
+
+function getMeetings() {
+	const now = moment();
+	const currentTime = now.toISOString();
+	const tenMinutesFromNow = now.clone().add(10, 'm').toISOString();
+
+	return googleCalendar.getMeetingWithinTimeframe(
+		currentTime,
+		tenMinutesFromNow,
+		config.timeZone
+	);
+}
+
+function getTemperature() {
+	return temperatureReader.getTemperature();
+}
+
+function outputTemp(meetings, temp) {
+	console.log(`current temp in meeting room is ${temp}`);
+	
+	let promise = Promise.resolve();
+
+	_.each(meetings, (meeting) => {
+		if (!updatedEvents[meeting.id]) {
+			promise = promise.then(() => {
+				return googleCalendar.updateEvent(meeting, temp)
+					.then(function() {
+						updatedEvents[meeting.id] = true;
+					});
+			});
+		}
+	});
+
+	return promise;
+}
+
+function updateMeetingInvites() {
+	return getMeetings()
+		.then(function(meetings) {
+			if (meetings.length > 0) {
+				return getTemperature()
+					.then(_.partial(outputTemp, meetings));
+			}
+
+			console.log('No meetings... Skipping');
+		})
+		.then(function(res) {
+			console.log('Finished updating');
+		})
+		.catch(function(err) {
+			console.error(`Unexpected error: ${err}`);
+		});
+}
+
+googleCalendar.authenticate(path.join(process.cwd(), 'authtoken'))
+	.then(function() {
+		updateMeetingInvites();
+
+		setInterval(updateMeetingInvites, INTERVAL_TIME);
+	});
